3a32dd222fe5f3c6287357daba6bc5bbfbad1568
Mahan <57597379+mdpe-ir@users.noreply.github.com>
2021-08-23 17:05:01 +0430

    Update device_screen_recorder.dart

--- lib/device_screen_recorder.dart
+++ lib/device_screen_recorder.dart
@@ -5,8 +5,8 @@ import 'package:flutter/services.dart';
 class DeviceScreenRecorder {
   static const MethodChannel _channel = const MethodChannel('device_screen_recorder');
 
-  static Future<bool?> startRecordScreen({String? name}) async {
-    final bool? version = await _channel.invokeMethod('startRecordScreen', {"name": name});
+  static Future<bool?> startRecordScreen({String? name , String? path}) async {
+    final bool? version = await _channel.invokeMethod('startRecordScreen', {"name": name , "path":path});
     return version;
   }
 
